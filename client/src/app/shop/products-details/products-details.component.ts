4a6f2e8e9aae7e98f453f98d03e7a803d9feff53
Diogo <dinash@outlook.com>
2022-11-03 17:32:23 -0300

    Api + Angular CLient

---
+++ client/src/app/shop/products-details/products-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-products-details',
+  templateUrl: './products-details.component.html',
+  styleUrls: ['./products-details.component.scss']
+})
+export class ProductsDetailsComponent implements OnInit {
+  product: IProduct;
+
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDetails', ' ');
+     }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({next: (product) =>{
+      this.product = product;
+      this.bcService.set('@productDetails', product.name)
+    }, error: (error) =>
+      console.log(error)
+    });
+  }
+
+}
---
+++ client/src/app/shop/shop-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
+import { ProductsDetailsComponent } from './products-details/products-details.component';
+
+const routes: Routes = [
+  {path: '', component: ShopComponent},
+  {path: ':id', component: ProductsDetailsComponent, data: {breadcrumb: {alias: 'productDetails'}}},
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class ShopRoutingModule { }
